a9938d7be0f25c6e8c7756f10e65a85aea65a4a2
GitHub Joey1904 <GitHub Joey1904@163.com>
2023-04-12 20:42:15 +0800

    git push

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `Joey Blog`;
+export const SITE_DESCRIPTION = '分享我经历过的事情、学习过的知识、发现的好东西。';
+export const SITE_EMAIL = 'joey1904@163.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
 export const SITE_LANG = "zh-CN";
